c618fe35fd6d8822139a31996c246338925c6c02
miguelj24 <pena87150@gmail.com>
2025-10-22 16:01:18 -0500

    encarpetado de payment y seat

--- FrontEnd/avian/Router.jsx
+++ FrontEnd/avian/Router.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router";
 import ClientLayout from "./src/layouts/ClientLayout";
 import VuelosPage from "./src/features/vuelos/pages/VuelosPage";
+import ReservationPage from "./src/features/reservations/pages/ReservationPage";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<ClientLayout />}>
         <Route path="/" element={<VuelosPage />} />
-        <Route path="reservas" />
+        <Route path="reserva" element={<ReservationPage />} />
         <Route path="asientos" />
         <Route path="pagos" />
         <Route path="asientos" />
